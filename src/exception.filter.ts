54e40ce527e60186449de3de0ef8925b97a5500d
Marcos Chalet <chaletmarcos@gmail.com>
2024-04-15 09:37:12 -0300

    fix: receive erros from DTO

--- src/exception.filter.ts
+++ src/exception.filter.ts
@@ -1,24 +1,35 @@
 import {
   ArgumentsHost,
+  BadRequestException,
   Catch,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-
 import { Response } from 'express';
 
 @Catch()
 export class ExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
+
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
+    if (exception instanceof BadRequestException) {
+      res.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        message: exception.getResponse()['message'] || 'Bad request error',
+      });
+      return;
+    }
+
     res.status(status).json({
       statusCode: status,
+      timestamp: new Date().toISOString(),
       message: exception.message || 'Internal server error',
     });
   }
