adb3d52975ef456653479bac529cfdf2a7a8dd2f
marcosChalet <marcoschalet.com.br>
2024-04-25 17:08:19 -0300

    fix: MAX_250_CHARACTERS message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -5,7 +5,7 @@ export const ErrorMessagesHelper = (fieldName: string) => {
     MAX_20_CHARACTERS: `The field ${fieldName} must be less than 20 characters`,
     MAX_50_CHARACTERS: `The field ${fieldName} must be less than 50 characters`,
     MAX_250_CHARACTERS: `The field ${fieldName} must be less than 250 characters`,
-    MAX_500_CHARACTERS: `The field ${fieldName} must be less than 250 characters`,
+    MAX_500_CHARACTERS: `The field ${fieldName} must be less than 500 characters`,
     MAX_1000_CHARACTERS: `The field ${fieldName} must be less than 1000 characters`,
     IS_NAN: `The field ${fieldName} must be a number`,
     NOT_BOOLEAN: `The field ${fieldName} must be a boolean`,
