3ee964ca4a604f320a8e1a00fac2e036c58ef1d2
marcosChalet <marcoschalet.com.br>
2024-04-25 20:42:54 -0300

    style: change description text example

--- initdb.sql
+++ initdb.sql
@@ -53,7 +53,9 @@ INSERT INTO "Product" ("name" , sku , category_id , description , large_descript
 500000, null, null, true, 'https://i.postimg.cc/3Jt9DLHr/sofa-pequeno.png', '', NOW()),
 
 ('Stellar Sofa', 'STE-002-9000000-BLU', 2, 'Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound.',
-'Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.\n\nWeighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.',
+'Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.
+
+Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.',
 9000000, null, null, false, 'https://i.postimg.cc/KYM5wx34/sofas.png', 'https://i.postimg.cc/XYD2ts21/miniatura1-sofa.png;https://i.postimg.cc/XYvH2Rgy/miniatura2-sofa.png;https://i.postimg.cc/DzJCh1Hv/miniatura3-sofa.png;https://i.postimg.cc/KY608Rhn/miniatura4-sofa.png', NOW()),
 
 ('Syltherine', 'SYL-001-3500000-WHI', 1, 'Stylish cafe chair',
@@ -89,7 +91,9 @@ INSERT INTO "Product" ("name" , sku , category_id , description , large_descript
 500000, null, null, true, 'https://i.postimg.cc/3Jt9DLHr/sofa-pequeno.png', '', NOW()),
 
 ('Stellar Sofa', 'STE-002-9000000-BLU', 2, 'Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound.',
-'Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.\n\nWeighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.',
+'Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.
+
+Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.',
 9000000, null, null, false, 'https://i.postimg.cc/KYM5wx34/sofas.png', 'https://i.postimg.cc/XYD2ts21/miniatura1-sofa.png;https://i.postimg.cc/XYvH2Rgy/miniatura2-sofa.png;https://i.postimg.cc/DzJCh1Hv/miniatura3-sofa.png;https://i.postimg.cc/KY608Rhn/miniatura4-sofa.png', NOW());
 
 
