3d0062a6c8f8a733067581a6f8a85adec43d72ef
marcosChalet <chaletmarcos@gmail.com>
2024-05-14 16:49:18 -0300

    chore: configure to development environment

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
