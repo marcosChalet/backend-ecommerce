929ff4a700c6a2ad26bab664ea90f648de1b9064
marcosChalet <chaletmarcos@gmail.com>
2024-05-26 09:46:18 -0300

    feat: enable logs

--- src/modules/global/prisma/prisma.service.ts
+++ src/modules/global/prisma/prisma.service.ts
@@ -4,6 +4,11 @@ import { PrismaClient } from '@prisma/client';
 @Global()
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['warn', 'error'],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
