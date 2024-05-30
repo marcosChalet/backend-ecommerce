ef4b3edff1c467a5951e24ad9d63c32f5706c6fb
marcosChalet <chaletmarcos@gmail.com>
2024-05-30 14:56:44 -0300

    chore: add cors origin data

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
